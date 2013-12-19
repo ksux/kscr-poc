5cb3104b248edf88d5e4e26996786d3832ef01b9
Chris Basham <csbasham@iu.edu>
2013-12-19 15:54:31 -0500

    Generated Angular project with Foundation for CSS.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ksCrPocApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
