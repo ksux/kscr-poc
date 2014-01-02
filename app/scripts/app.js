c6cfb2d3585d578123a3a4290b911d223b483f47
Chris Basham <csbasham@iu.edu>
2014-01-02 17:29:06 -0500

    Worked on state transitions, including sibling next/prev links in results.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
 angular.module('kscrPocApp', [
   //'ngCookies',
   //'ngResource',
-  //'ngSanitize',
+  'ngSanitize',
   //'ngRoute',
   'ngTouch',
   'ui.router'
@@ -16,9 +16,54 @@ angular.module('kscrPocApp', [
         abstract: true,
         templateUrl: 'partials/app.html',
         controller: function($scope, TermsService) {
-          $scope.selectedSearchTerm = '201301';
+          $scope.searchCriteria = {
+            selectedTerm: '201301',
+            query: ''
+          };
           $scope.searchTerms = TermsService;
-          $scope.showSearch = true;
+          $scope.showSearch = false;
+        }
+      })
+      .state('app.search', {
+        abstract: true,
+        template: '<div ui-view></div>'
+      })
+      .state('app.search.query', {
+        url: '/search',
+        templateUrl: 'partials/app.search.query.html',
+        data: {
+          title: 'Search'
+        },
+        controller: function($scope, $state) {
+          $scope.search = function() {
+            console.log($scope.searchCriteria);
+            $state.go('app.search.results.list');
+          };
+        }
+      })
+      .state('app.search.results', {
+        abstract: true,
+        url: '/results',
+        templateUrl: 'partials/app.search.results.html'
+      })
+      .state('app.search.results.list', {
+        url: '',
+        templateUrl: 'partials/app.search.results.list.html',
+        data: {
+          title: 'Results'
+        },
+        controller: function($scope, SearchResultsService) {
+          $scope.results = SearchResultsService.results;
+        }
+      })
+      .state('app.search.results.details', {
+        url: '/:index/:code',
+        templateUrl: 'partials/app.search.results.details.html',
+        controller: function($scope, $stateParams, SearchResultsService) {
+          $scope.itemCount = SearchResultsService.count;
+          $scope.previousItem = SearchResultsService.previous($stateParams.index);
+          $scope.nextItem = SearchResultsService.next($stateParams.index);
+          $scope.item = SearchResultsService.item($stateParams.index);
         }
       })
       .state('app.schedule', {
@@ -30,28 +75,10 @@ angular.module('kscrPocApp', [
         controller: function($scope, ScheduleService) {
           $scope.schedule = ScheduleService;
         }
-      })
-      .state('app.results', {
-        abstract: true,
-        template: '<div ui-view></div>'
-      })
-      .state('app.results.list', {
-        url: '/results',
-        templateUrl: 'partials/app.results.list.html',
-        data: {
-          title: 'Results'
-        },
-        controller: function($scope, SearchResultsService) {
-          $scope.results = SearchResultsService;
-        }
-      })
-      .state('app.results.details', {
-        url: '/details',
-        templateUrl: 'partials/app.results.details.html'
       });
 
     // For any unmatched url, send to a default route
-    $urlRouterProvider.otherwise('/schedule');
+    $urlRouterProvider.otherwise('/search');
   })
   .run(function ($rootScope, $state) {
     $rootScope.$state = $state;
@@ -66,4 +93,34 @@ angular.module('kscrPocApp', [
       }
       $rootScope.pageTitle = titles.join(' - ');
     });
+
+    // Until the `ui-sref` directive allows dynamic state references,
+    // we need to manually store and trigger dynamic states.
+    // https://github.com/angular-ui/ui-router/issues/395
+
+    // TODO
+    // Perhaps a full history could be stored for each state,
+    // instead of only one-level back.
+
+    // List of all dynamic state references.
+    $rootScope.srefs = {};
+
+    // Register a dynamic state reference.
+    var registerSref = function(baseStateName, toState, toParams) {
+      $rootScope.srefs[baseStateName] = $state.href(toState, toParams);
+    };
+
+    // Registering the default dynamic states.
+    registerSref('app.search', 'app.search.query');
+
+    // Whenver the state changes, override the dynamic href
+    // generated for any of the decendants of the base state.
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams) {
+      angular.forEach($rootScope.srefs, function(value, key) {
+        if( $state.includes(key) ) {
+          registerSref(key, toState, toParams);
+        }
+      });
+    });
+
   });
