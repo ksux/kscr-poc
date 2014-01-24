89d0ff8ff1f5f313b1c23fe9a32f4b9aa7b4b32d
Chris Basham <csbasham@iu.edu>
2014-01-24 11:14:48 -0500

    Removed schedule duplicates in the API response, until the duplicates are prevented in the API.

--- app/modules/shared/services/schedule-service.js
+++ app/modules/shared/services/schedule-service.js
@@ -6,7 +6,25 @@ angular.module('kscrPocApp')
       query: {
         method: 'GET',
         cache: true,
-        isArray: true
+        isArray: true,
+        transformResponse: function(data) {
+          // Convert the raw data string to native objects.
+          data = angular.fromJson(data);
+
+          // Process the Activity Offerings, retaining only the first schedule.
+          // This is no longer needed once the service prevents duplicates.
+          // https://jira.kuali.org/browse/KSENROLL-11632
+          angular.forEach(data, function(scheduleItem) {
+            angular.forEach(scheduleItem.activityOfferings, function(ao) {
+              var sc = ao.scheduleComponents;
+              if( angular.isArray(sc) && angular.isDefined(sc[0]) ) {
+                ao.schedule = sc[0];
+              }
+            });
+          });
+
+          return data;
+        }
       }
     });
   });
