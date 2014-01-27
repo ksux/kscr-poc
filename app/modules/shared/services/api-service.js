42bf849a1bbd8d496c51a69e0e8b41505e89022d
dan gillen <dan.gillen@utah.edu>
2014-01-27 11:53:43 -0700

    More cleanup.

---
+++ app/modules/shared/services/api-service.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('kscrPocApp')
+  .factory('apiService', function (config) {
+    return {
+      get: function(endpoint) {
+        var url = config.apiBase;
+        // Set the appropriate namespace.
+        switch(endpoint) {
+          case 'personschedule':
+            url += config.apiCourseRegistrationBase;
+            break;
+          default:
+            url += config.apiScheduleOfClassesBase;
+            break;
+        }
+        // Set for a second level.
+        switch(endpoint) {
+          case 'primaryactivities':
+            url += 'courseofferings/';
+            break;
+          default:
+            break;
+        }
+        url += endpoint;
+        return url;
+      }
+    };
+  });
